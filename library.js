c31e598ee753eec334e10c38808cf529df541249
Sean Huan Zhi Tan <u7599095@anu.edu.au>
2023-12-13 21:02:14 +0800

    Added GameState class

--- library.js
+++ library.js
@@ -1,3 +1,19 @@
+class GameState {
+    constructor() {
+        // board is an 8 * 8 array containing pieces
+        this.board = Array(8).fill(null).map(() => Array(8).fill(null));
+        // isWhiteTurn keeps track of who should make the next move
+        this.isWhiteTurn = true;
+    }
+    
+    /**
+     * Adds pieces to the board as an initial setup for the game.
+     */
+    populateBoard() {
+        // TODO: Implement the logic to add chess pieces to the board
+    }
+}
+
 class Piece { 
     static types = Object.freeze({
         king: 0,
